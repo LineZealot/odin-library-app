a63b78b5f3e5275e5f89c35d0b8ed1b3f200ea61
Holden Smith <105118057+LineZealot@users.noreply.github.com>
2023-05-22 09:49:35 -0500

    Eslint and Prettier setup

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
 function library() {
-    const bookshelf = document.getElementById('bookshelf');
+  const bookshelf = document.getElementById("bookshelf");
 
-    let myLibrary = [];
+  const myLibrary = [];
 
-    function Book(title, author, pages, read) {
-        this.title = title,
-        this.author = author,
-        this.pages= pages,
-        this.read = read,
-        this.info = function() {
-            return title + ' by ' + author + ' with ' + pages + ' pages, ' + read;
-        }
-    };
+  function Book(title, author, pages, read) {
+    (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.info = function () {
+        return `${title} by ${author} with ${pages} pages, ${read}`;
+      });
+  }
 
-    function createBookElement(b) {
-        const box = document.createElement("div");
+  function createBookElement(b) {
+    const box = document.createElement("div");
 
-        const textHeading = document.createElement("h3");
-        textHeading.textContent = b.title;
+    const textHeading = document.createElement("h3");
+    textHeading.textContent = b.title;
 
-        const textBody = document.createElement('p');
-        textBody.textContent = 'Written by ' + b.author + ' has ' + b.pages + ' pages, ' + b.read;
-        
-        box.appendChild(textHeading);
-        box.appendChild(textBody);
-        
-        bookshelf.appendChild(box);
-    };
+    const textBody = document.createElement("p");
+    textBody.textContent = `Written by ${b.author} has ${b.pages} pages, ${b.read}`;
 
-    function addBookToLibrary(x) {
-        myLibrary.push(x);
-    };
+    box.appendChild(textHeading);
+    box.appendChild(textBody);
 
-    const newBookButton = document.getElementById('book-button');
+    bookshelf.appendChild(box);
+  }
 
-    newBookButton.addEventListener('click', () => {
-        const newBook = new Book(prompt(), prompt(), prompt(), prompt());
-        addBookToLibrary(newBook);
-        createBookElement(newBook);
-        console.log(myLibrary);
-    })
+  function addBookToLibrary(x) {
+    myLibrary.push(x);
+  }
 
-    // test existing books
-    const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295', 'already read.');
-    addBookToLibrary(theHobbit);
+  const newBookButton = document.getElementById("book-button");
 
-    // append existing books into the DOM
-    for(let x = 0; x < myLibrary.length; x++) {
-        createBookElement(myLibrary[x]);
-    }
+  newBookButton.addEventListener("click", () => {
+    const newBook = new Book(prompt(), prompt(), prompt(), prompt());
+    addBookToLibrary(newBook);
+    createBookElement(newBook);
+    console.log(myLibrary);
+  });
+
+  // test existing books
+  const theHobbit = new Book(
+    "The Hobbit",
+    "JRR Tolkien",
+    "295",
+    "already read."
+  );
+  addBookToLibrary(theHobbit);
+
+  // append existing books into the DOM
+  for (let x = 0; x < myLibrary.length; x += 1) {
+    createBookElement(myLibrary[x]);
+  }
 }
 library();
